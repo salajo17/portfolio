26335223a099cefa2d853cbc350ab78bd1d401b2
Salayo Din <salajo17@gmail.com>
2022-11-24 23:00:32 -0500

    setting everything up

---
+++ hw.js
@@ -0,0 +1,10 @@
+var list = ["cheesePizza", "pepperoniPizza"]
+
+var dict = {
+    username: "Lorky",
+    password: "123456789"
+}
+
+for (var i = 0; i < list.length; i++) {
+    console.log(list[i], i)
+}
